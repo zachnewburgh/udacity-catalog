cea5dccd3775cd0ebb008a9e29037b8768bd9fa8
zachnewburgh <zach.newburgh@gmail.com>
2017-01-03 13:31:52 -0500

    Created catalog_populator to instantiate dummy Category

---
+++ catalog_populator.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from catalog_database import Base, Category, CategoryItem
+
+engine = create_engine('sqlite:///catalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Create dummy categories
+Category1 = Category(name='Baseball')
+session.add(Category1)
+session.commit()
+
+
+print "Populated the catalog database!"
