000eb67ebf981bcabec7804997ed795aae0a3f12
zachnewburgh <zach.newburgh@gmail.com>
2017-01-05 19:32:23 -0500

    Added serialize_items() and items (with backref) to Category, and removed category from CategoryItem

--- catalog_database.py
+++ catalog_database.py
@@ -9,22 +9,26 @@ class Category(Base):
   __tablename__ = 'category'
   id = Column(Integer, primary_key=True)
   name = Column(String(250), nullable=False)
+  items = relationship("CategoryItem", backref="category")
 
   @property
   def serialize(self):
       return {
         'id': self.id,
         'name': self.name,
-        'Item': self.items
+        'Item': [item.serialize for item in self.items]
       }
+
+  @property
+  def serialize_items(self):
+    return [item.serialize for item in self.items]
 
 class CategoryItem(Base):
   __tablename__ = 'category_item'
   id = Column(Integer, primary_key = True)
   title = Column(String(80), nullable = False)
   description = Column(String(250))
-  cat_id = Column(Integer,ForeignKey('category.id'))
-  category = relationship(Category)
+  cat_id = Column(Integer, ForeignKey('category.id'))
 
   @property
   def serialize(self):
